423d87c0123dbee58123007ad60262c89da414a2
MarlynMuchina <marlyn.muchina@strathmore.edu>
2024-10-23 15:52:34 +0300

    ajax

---
+++ ajax.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    load_data();
+
+    function load_data(query){
+        $.ajax({
+            url:"process_data.php",
+            method:"POST",
+            data:{ query:query},
+            success:function(data){
+                $('#out_put').html(data);
+            }
+
+        });
+    }
+    $('#in_put').keyup(function(){
+        var search = $(this).val();
+        if(search != ''){
+            load_data(search);
+        }else{
+            load_data();
+        }
+
+    });
+
+
+});
+
+
+
